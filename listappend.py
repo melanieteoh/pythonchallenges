ebc84576f90281b2c6ce70a01be89c231e1c707f
melanieteoh <92512366+melanieteoh@users.noreply.github.com>
2022-10-07 22:24:55 +0800

    Create listappend.py

---
+++ listappend.py
@@ -0,0 +1,4 @@
+MyList = ["mario", "luigi", "princess peach", "yoshi", "toad", "flash", "supergirl", "green lantern"]
+NewMember = input("Please add a video game character.")
+MyList.append(NewMember)
+print(MyList)
