f9582c456b1f59e64267cdb02bfe2a484c3037f8
melanieteoh <92512366+melanieteoh@users.noreply.github.com>
2022-10-07 22:25:48 +0800

    Create listcommands.py

---
+++ listcommands.py
@@ -0,0 +1,6 @@
+MyList = [2,4,6,8,10,12,14,16,18,20]
+print("Total:", sum(MyList))
+print("Minimum:", min(MyList))
+print("Maximum:", max(MyList))
+print("Length:", len(MyList))
+print("Mean Average:", sum(MyList)/len(MyList))
