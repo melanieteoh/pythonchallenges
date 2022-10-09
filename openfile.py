368f976d534e23f208b060fd83412648e080cbc4
melanieteoh <92512366+melanieteoh@users.noreply.github.com>
2022-10-10 01:04:19 +0800

    Create openfile.py

---
+++ openfile.py
@@ -0,0 +1,7 @@
+with open("rudyard.txt","r") as whole_file:
+   for line in whole_file:
+        print(line,end="")
+
+with open("mam.txt","r") as whole_file:
+   for line in whole_file:
+        print(line,end="")
